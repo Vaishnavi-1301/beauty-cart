f87c5f7c04d483c0436fd3d6967ae6f2734274cb
Vaishnavi-1301 <vaishnavishirsat01@gmail.com>
2025-04-10 05:15:49 +0530

    Add files via upload

---
+++ adminLogin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<title>BeautyCart Admin</title>
+</head>
+
+<body>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div>
+	<h3 align="center">Login as administrator</h3>
+    <form method="post" action="adminController.php">
+	<table align="center" cellspacing="10px">
+		<tr>
+			<td>User Name</td>
+			<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td><input type="password" name="password"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" value="Log In"></td>
+		</tr>
+
+	</table>
+	</form>
+</div>
+</body>
